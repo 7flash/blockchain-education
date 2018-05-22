0d156472c7d5bd0cbaeb5df664c34adb19fbe1eb
root <root@ASR-DEL3>
2018-05-22 22:27:20 +0300

    init

---
+++ education/test/CourseRegistry.js
@@ -0,0 +1,33 @@
+contract("CourseRegistry", function([creator]) {
+   before(async function() {
+
+   });
+
+   it("should encrypt link with author's public key", async function() {
+
+   });
+
+   it("should save encrypted link", async function() {
+
+   });
+
+   it("should receive token associated with encrypted link", async function() {
+
+   });
+
+   it("should decrypt link from token", async function() {
+
+   });
+
+   it("should encrypt link with student's public key", async function() {
+
+   });
+
+   it("should create children token associated with parent", async function() {
+
+   });
+
+   it("should decrypt link from children token by student", async function() {
+
+   });
+});
