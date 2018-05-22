deeb82a6498a53ed416d2dda18faf65226800354
root <root@ASR-DEL3>
2018-05-23 01:29:09 +0300

    test

--- education/contracts/CourseRegistry.sol
+++ education/contracts/CourseRegistry.sol
@@ -1,35 +1,21 @@
 pragma solidity ^0.4.23;
 
+import "./Course.sol";
 
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
-import "./ERC998PossessERC721.sol";
+contract CourseRegistry {
+    address[] courses;
 
-contract CourseRegistry is ERC721Token, ERC998PossessERC721 {
-    constructor(string _name, string _symbol) public ERC721Token(_name, _symbol) {}
-
-    mapping (uint256 => address) courses;
-
-    function mint721(address _to)
-        public returns(uint256)
+    function createCourse(address _author, string _name)
+        public
     {
-        _mint(_to, allTokens.length + 1);
-        return allTokens.length;
+        address createdCourse = new Course(_author, _name);
+
+        courses.push(createdCourse);
     }
 
-    function _owns(address _owner, uint256 _tokenId)
-        internal view returns(bool)
+    function findCourseByID(uint256 _id)
+        public constant returns (address)
     {
-        return (tokenOwner[_tokenId] == _owner);
-    }
-
-    function safeTransferNFTP(
-        address _to, uint256 _tokenId, address _nftpContract, uint256 _nftpTokenId, bytes _data
-    ) public {
-
-        require(_owns(msg.sender, _tokenId));
-
-        transferNFTP(_to, _tokenId, _nftpContract, _nftpTokenId);
-
-        nftpReceived(_nftpContract, _nftpTokenId, _data);
+        return courses[_id];
     }
 }
--- education/test/CourseRegistry.js
+++ education/test/CourseRegistry.js
@@ -1,33 +1,36 @@
-contract("CourseRegistry", function([creator]) {
+const CourseRegistry = artifacts.require("./CourseRegistry");
+const Course = artifacts.require("./Course");
+
+const utils = require("./utils");
+const expect = utils.expect;
+const expectThrow = utils.expectThrow;
+
+const EthCrypto = require("eth-crypto");
+
+contract("CourseRegistry", function([deployer, author, student]) {
+   const link = "yandex.ru";
+
    before(async function() {
+      this.registry = await CourseRegistry.new("Hackaton", "WIN", { from: deployer });
 
+      await this.registry.createCourse(author, "Blockchain Introduction", { from: deployer });
+
+      this.course = Course.at(await this.registry.findCourseByID(0));
    });
 
-   it("should encrypt link with author's public key", async function() {
-
+   it("should be initialized correctly", async function() {
+      expect(await this.course.name()).to.be.equal("Blockchain Introduction");
+      expect(await this.course.symbol()).to.be.equal("COURSE");
+      expect(await this.course.author()).to.be.equal(author);
    });
 
-   it("should save encrypted link", async function() {
-
+   it("should fail to create ticket from non-author", async function() {
+      await expectThrow(this.course.createStudentTicket(student, link, { from: deployer }));
    });
 
-   it("should receive token associated with encrypted link", async function() {
+   it("should create ticket with encrypted link", async function() {
+      await this.course.createStudentTicket(student, link, { from: author });
 
-   });
-
-   it("should decrypt link from token", async function() {
-
-   });
-
-   it("should encrypt link with student's public key", async function() {
-
-   });
-
-   it("should create children token associated with parent", async function() {
-
-   });
-
-   it("should decrypt link from children token by student", async function() {
-
+      expect(await this.course.tokenURI(1)).to.be.equal(link);
    });
 });
